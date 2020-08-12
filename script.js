5c295358f7aa2a68f00ebb9eab909e853d51e637
Oleg <artoleg99@gmail.com>
2020-08-12 12:10:43 +0300

    lesson 4

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 'use strict';
 
-var leftBorderWidth = 1;
-let second = 2;
-const pi = 3.14;
-console.log(second);
+
+var number =5;
+var string ="hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log ('string'*4);
+
+let something;
+console.log(something);
+
+let person = {
+    name: "john",
+    age: 25,
+    isMarried: false
+};
+console.log(person.["name"]);
+
+let arr = ['plum.png','orange.jpeg','apple.bnp']
+
+console.log (arr[0])
