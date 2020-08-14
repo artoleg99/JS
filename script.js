4bd041caeabb7214bedf9f0f6df782e2dd216cec
Oleg <artoleg99@gmail.com>
2020-08-14 14:43:56 +0300

    lesson 6

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
+'use strict'
 let arr = ['plum.png', 'orange.jpeg', 'apple.bmp'];
 
-console.log(arr[0]);
+// console.log(arr[0]);
 
- alert("hello"); 
+// alert("hello"); 
 
-let answer = confirm("Are you here?");
+// let answer = confirm("Are you here?");
 
-console.log(answer);
+// console.log(answer);
 
-let answer = promt("Есть ли вам 18?", "Да");
+// let answer = +prompt("Есть ли вам 18?", "Да"); 
 
-console.log(typeof(null))
+// console.log(typeof(answer));
+
+// console.log("arr"+ " - object");
+// console.log(4 + + " - object");
+let incr = 10,
+    descr = 10;
+
+console.log(incr++);
+console.log(descr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked || !isClose);
+
+
+
