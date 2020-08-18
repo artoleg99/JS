dc84330dc5c9a67a129fe36f534c1bea7fcf37d9
Oleg <artoleg99@gmail.com>
2020-08-18 14:08:06 +0300

    lesson 9

--- script.js
+++ script.js
@@ -1,36 +1,21 @@
 'use strict';
 
-if (2*4 == 7) {
-    console.log("верно!")
-} else {
-    console.log("Неверно!")
-
 let num = 50;
 
-if (num <49) {
-    console.log("неверно!")
-} else if (num > 100) {
-    console.log("Много!")
-} else {
-    console.log("Верно!")
-};
+while (num < 55) {
+    console.log(num);
+    num++;
+}
 
-(num == 50) ? console.log("верно!") : console.log('Неверно!');
+do {
+    console.log(num);
+    num++;
+}
+while (num <55);
 
-switch (num) {
-    case num < 49:
-        console.log("неверно!");
-        break;
-    case num > 100:
-        console.log("Много!");
-        break;
-    case num > 80:
-        console.log("Много!");
-        break;
-    case 50:
-        console.log("Верно!");
-        break;
-    default:
-        console.log("Что-то пошло не так!")
-        break;
+for (let i = 1; i < 8; i++) {
+    if (i ==6){
+        continue
+    }
+    console.log(i)
 }
