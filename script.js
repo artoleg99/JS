d778b37734afe0de6fa589bec754bc20ecb9d363
Oleg <artoleg99@gmail.com>
2020-09-08 19:22:40 +0300

    lesson 12

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-function first(){
-    //что-то делаем
-    setTimeout( function(){
-        console.log(1);
-    }, 500);
+let options = {
+    width: 1024,
+    height: 1024,
+    name: "test"
+};
+
+console.log(options.name);
+options.bool = false;
+
+options.colors = {
+    border: "black",
+    bg: "red"
+};
+
+delete options.bool;
+
+console.log(options);
+
+for (let key in options) {
+    console.log('Свойство ' + key + ' имеет значение ' + options[key]);
 }
 
-function second(){
-    console.log(2);
-}
-
-first();
-second();
-
-function learnJS(lang, callback){
-    console.log("я учу " + lang);
-    callback();
-}
-
-function done() {
-    console.log("я прошел 3й урок");
-}
-
-learnJS("JavaScript", done);
+console.log(Object.keys(options).length);
