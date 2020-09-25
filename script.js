6065350dea8d5a75f2804785bbf126e94f4d85e6
Oleg <artoleg99@gmail.com>
2020-09-25 11:19:26 +0300

    lesson 17

--- script.js
+++ script.js
@@ -4,12 +4,29 @@ let box = document.getElementById('box'),
     heart = document.querySelectorAll('.wrapper .heart'),
     oneHeart = document.querySelector('.heart');
 
+box.style.backgroundColor = 'blue';
+btn[1].style.borderRadius = '100%';
 
+circle[0].style.backgroundColor = 'red';
+circle[1].style.backgroundColor = 'yellow';
+circle[2].style.backgroundColor = 'green';
 
-console.log(box);
-console.log(btn);
-console.log(btn[0]);
-console.log(circle[2]);
-console.log(heart[1]);
-console.log(oneHeart);
+// for (let i = 0; i < heart.length; i++) {
+//     heart[i].style.backgroundColor = 'blue';
+// }
 
+// heart.forEach(function(item, i , hearts) {
+//     item.style.backgroundColor = 'blue';
+// });
+
+let div = document.createElement('div'),
+    text = document.createTextNode('тут был я');
+div.textContent = 'hello world!';
+
+document.body.insertBefore(div, circle[0]);
+document.body.removeChild(circle[1]);
+wrapper.removeChild(heart[1]);
+
+document.body.replaceChild(heart[1]);
+
+console.log(div);
