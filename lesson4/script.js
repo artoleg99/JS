fe9cb97e547a3a095bf8ae74ea45cf37ed34fd22
Oleg <artoleg99@gmail.com>
2020-10-22 22:40:59 +0300

    lessons 24-29

--- lesson4/script.js
+++ lesson4/script.js
@@ -1,86 +1,101 @@
-// 1)
-function User(name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-    this.hello = function() {
-        console.log('hello' + this.name);
-    };
+//lesson 24
+
+let name = 'Ivan',
+    age = 30,
+    mail = 'ex@mail.ru';
+
+document.write(`пользователю ${name} ${age} лет. его почтовый адрес: ${mail}`);
+
+//lesson 25
+
+function makeArray() {
+    var items = [];
+
+    for (let i = 0; i < 10; i++) {
+        var item = function() {
+            console.log(i);
+        };
+        items.push(item);
+    }
+
+    return items;
 }
 
-User.prototype.exit = function(name) {
-    console.log('пользователь ' + this.name +' ушел');
+var arr = makeArray();
+
+arr[1]();
+arr[3]();
+arr[7]();
+
+//lesson 26
+
+let fun = () => {
+    console.log(this);
 };
 
-let ivan = new User('Ivan', 25),
-    alex = new User('Alex', 20);
+// fun();
 
-console.log(ivan);
-console.log(alex);
-
-ivan.exit();
-
-// 2)
-function showThis(a, b) {
-    console.log(this);
-    function sum() {
-        console.log(this);
-        return a + b;
-    }
-    console.log(sum());
-}
-showThis(4, 5);
-showThis(5, 5);
-
-// 3)
 let obj = {
-    a: 20,
-    b: 15,
-    sum: function() {
-        console.log(this);
-        function shout() {
+    number: 5,
+    sayNumber: function() {
+        let say = () => {
             console.log(this);
-        }
-        shout();
+        };
+        say();
     }
 };
-obj.sum();
 
-// 4)
-
-let user = {
-    name: "John"
-};
-
-function sayName(surname) {
-    console.log(this);
-    console.log(this.name + surname);
-
-}
-
-console.log(sayName.call(user, ' Smith'));
-console.log(sayName.apply(user, [' Snow']));
-
-function count(number) {
-    return this * number;
-}
-
-let double = count.bind(2);
-console.log(double(3));
-console.log(double(10));
+obj.sayNumber();
 
 let btn = document.querySelector('button');
 
 btn.addEventListener('click', function() {
-    console.log(this);
-    this.style.backgroundColor = 'red';
-    function showThis() {
-        console.log(this); 
-    }
-    showThis();
+    let show = () => {
+        console.log(this);
+    };
+    show();
 });
 
-// 1) просто вызов функции - window/undefined
-// 2) метод объекта - this = объект
-// 3) конструктор (new) - this = новый созданный объект
-// 4) указание конкретного контекста - call, apply, bind
+// lesson 27
+
+function caclOrDouble (number, basis = 2) {
+    // basis = basis || 2; ES5
+    console.log(number * basis);
+}
+caclOrDouble(3, 5);
+caclOrDouble(6);
+
+// lesson 28
+
+class Rectangle {
+    constructor(height, width = 20) {
+        this.height = height;
+        this.width = width;
+    }
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+const square = new Rectangle(10);
+
+console.log(square.calcArea());
+
+// lesson 29
+
+let video = ['youtube', 'vimeo', 'rutube'],
+    blogs = ['wordpress', 'livejornal', 'blogger'],
+    internet = [...video, ...blogs, 'vk', ' facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(a + b + c);
+}
+
+let numbers = [2, 5, 7];
+
+log(...numbers);
