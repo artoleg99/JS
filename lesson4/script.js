550cb2f10d9677552aa92593d7508ff08c5240b8
Oleg <artoleg99@gmail.com>
2020-10-24 19:38:43 +0300

    lesson 31

--- lesson4/script.js
+++ lesson4/script.js
@@ -1,11 +1,27 @@
-let options = {
-    width: 1366,
-    height: 768,
-    background: 'red',
-    font: {
-        size: '16px',
-        color: '#fff'
-    }
-};
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
 
-console.log(JSON.parse(JSON.stringify(options)));
+inputRub.addEventListener('input', () => {
+    let request = new XMLHttpRequest();
+
+    // request.open(method, url, async, login, pass);
+    request.open('GET', 'current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send(); 
+
+    // status отвечает, в каком состоянии сервер (404)
+    // statusText содержит текстовое описание ответа сервера
+    // responseText содержит текст ответа сервера /response
+    // readyState  содержит текущее состояние запроса
+
+    request.addEventListener('readystatechange', function() {
+        if (request.readyState === 4 && request.status == 200) {
+            let data = JSON.parse(request.response);
+
+            inputUsd.value = inputRub.value / data.usd;
+        } else {
+            inputUsd.value = "Что-то пошло не так!";
+        }
+    });
+
+}); 
