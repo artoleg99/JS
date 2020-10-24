7f7cff4df2371b9a688fd574ebd6bd8f41fb189b
Oleg <artoleg99@gmail.com>
2020-10-24 19:03:27 +0300

    lesson 30

--- lesson4/script.js
+++ lesson4/script.js
@@ -1,101 +1,11 @@
-//lesson 24
-
-let name = 'Ivan',
-    age = 30,
-    mail = 'ex@mail.ru';
-
-document.write(`пользователю ${name} ${age} лет. его почтовый адрес: ${mail}`);
-
-//lesson 25
-
-function makeArray() {
-    var items = [];
-
-    for (let i = 0; i < 10; i++) {
-        var item = function() {
-            console.log(i);
-        };
-        items.push(item);
-    }
-
-    return items;
-}
-
-var arr = makeArray();
-
-arr[1]();
-arr[3]();
-arr[7]();
-
-//lesson 26
-
-let fun = () => {
-    console.log(this);
-};
-
-// fun();
-
-let obj = {
-    number: 5,
-    sayNumber: function() {
-        let say = () => {
-            console.log(this);
-        };
-        say();
+let options = {
+    width: 1366,
+    height: 768,
+    background: 'red',
+    font: {
+        size: '16px',
+        color: '#fff'
     }
 };
 
-obj.sayNumber();
-
-let btn = document.querySelector('button');
-
-btn.addEventListener('click', function() {
-    let show = () => {
-        console.log(this);
-    };
-    show();
-});
-
-// lesson 27
-
-function caclOrDouble (number, basis = 2) {
-    // basis = basis || 2; ES5
-    console.log(number * basis);
-}
-caclOrDouble(3, 5);
-caclOrDouble(6);
-
-// lesson 28
-
-class Rectangle {
-    constructor(height, width = 20) {
-        this.height = height;
-        this.width = width;
-    }
-    calcArea() {
-        return this.height * this.width;
-    }
-}
-
-const square = new Rectangle(10);
-
-console.log(square.calcArea());
-
-// lesson 29
-
-let video = ['youtube', 'vimeo', 'rutube'],
-    blogs = ['wordpress', 'livejornal', 'blogger'],
-    internet = [...video, ...blogs, 'vk', ' facebook'];
-
-console.log(internet);
-
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-    console.log(a + b + c);
-}
-
-let numbers = [2, 5, 7];
-
-log(...numbers);
+console.log(JSON.parse(JSON.stringify(options)));
