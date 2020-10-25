9cf00ca48cb38313c13d8b0e9f5590aea78bb99a
Oleg <artoleg99@gmail.com>
2020-10-25 20:01:51 +0300

    homework 14

--- lesson4/script.js
+++ lesson4/script.js
@@ -1,27 +1,35 @@
+
 let inputRub = document.getElementById('rub'),
     inputUsd = document.getElementById('usd');
 
-inputRub.addEventListener('input', () => {
-    let request = new XMLHttpRequest();
+    function catchData() {
 
-    // request.open(method, url, async, login, pass);
-    request.open('GET', 'current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    request.send(); 
+        return new Promise(function(resolve, reject){
+            let request = new XMLHttpRequest();
+            request.open("GET", "js/current.json");
 
-    // status отвечает, в каком состоянии сервер (404)
-    // statusText содержит текстовое описание ответа сервера
-    // responseText содержит текст ответа сервера /response
-    // readyState  содержит текущее состояние запроса
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            request.send();
 
-    request.addEventListener('readystatechange', function() {
-        if (request.readyState === 4 && request.status == 200) {
-            let data = JSON.parse(request.response);
+            request.onload = function() {
+                if(request.readyState === 4) {
+                        if(request.status == 200) {
+                            resolve(this.response);
+                        }
+                        else {
+                            reject();
 
-            inputUsd.value = inputRub.value / data.usd;
-        } else {
-            inputUsd.value = "Что-то пошло не так!";
-        }
-    });
+                        }
+                }
+            };
+        });
+    }
 
-}); 
+catchData()
+.then(response => {
+    console.log(response);
+    let data = JSON.parse(response);
+    inputUsd.value = inputRub.value / data.usd;
+})
+.then(() => console.log(5000))
+.catch(() => inputUsd.value = "Что-то пошло не так");
