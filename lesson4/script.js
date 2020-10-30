f3a47c37836c26e809269c8f02da1a4649554d3c
Oleg <artoleg99@gmail.com>
2020-10-30 23:48:07 +0300

    lesson 37

--- lesson4/script.js
+++ lesson4/script.js
@@ -1,33 +1,30 @@
-function User (name, age) {
-    this.name = name;
-    let userAge = age;
+// let app = {
+//     date:45
+// };
+// console.log(app);
 
-    this.getAge = function() {
-        return userAge;
+let number = 1;
+
+(function() {
+    let number = 2;
+    console.log(number);
+
+    return console.log(number + 3);
+}());
+
+console.log(number);
+
+let user = (function() {
+    let privat = function() {
+        console.log(' im privat');
     };
-
-    this.setAge = function(age) {
-        if (typeof age === 'number' && age > 0 && age < 110) {
-            userAge = age;
-        } else {
-            console.log('недопустимое значение');
-        }
+    let sayHello = function() {
+        console.log('hello');
     };
-    this.say = function() {
-        console.log(`Имя пользователя ${this.name}, возраст ${this.userAge}`);
-
+    return {
+        sayHello: sayHello
     };
-}
+}());
 
-let user = new User('ivan', 25);
-console.log(user.name);
-console.log(user.userAge);
-user.say();
-console.log(user.getAge());
-user.setAge('30');
-console.log(user.getAge());
-
-
-// user.age = 30;
-// user.name = 'Alex';
-// user.say();
+console.log(user);
+console.log(user.sayHello());
